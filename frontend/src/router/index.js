bac52c116137bab915816b63cc154011fcb2a158
afonso.araujo <afonso.araujo@supertroco.com.br>
2025-03-06 13:31:17 -0300

    feat: configurando rota

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import TasksView from '../views/TasksView.vue'
+
+import { getAuth } from 'firebase/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,8 +12,34 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-    }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/tasks',
+      name: 'tasks',
+      component: TasksView,
+      meta: {
+        requiresAuth: true,
+      },
+    },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    const auth = getAuth()
+    if (auth.currentUser) {
+      next()
+    } else {
+      next('/login')
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
